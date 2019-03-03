1b432062005e028655c28263528c2968b7e9d77e
GRUPOGMV\jmpt <jmpt@gmv.es>
2019-03-03 21:17:05 +0100

    Upload first example, parse web & filter by body params

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # web2m3c
-Scrapping web to find m3c files links
+An scrapping application based on nodejs for Google cloud functions example.
+It parses the https://acelisting.in/ web in order to get show links for streaming watching.
+
+To filter shows use the following body example in a POST request:
+```
+{
+	time: '22:30',
+	date: '20 Mar',
+	sport: 'soccer',
+	match: 'liverpool',
+	league: 'premier league'
+}
+```
---
+++ index.js
@@ -0,0 +1,100 @@
+const cheerio = require('cheerio')
+const rp = require('request-promise');
+
+const acestreamHostStreamUrl = 'http://192.168.0.20:6878/ace/manifest.m3u8?id=';
+const html = 'https://acelisting.in/';
+
+exports.fetchMatches = (req, res) => {
+
+  let options = {
+    method: 'GET',
+    json: true,
+    uri: html,
+    insecure: true
+  };
+
+  rp(options)
+    .then(function (html) {
+      response = [];
+      const $ = cheerio.load(html);
+      rows = $('table.table.table-striped.table-bordered.table-condensed > tbody > tr', html);
+
+      for (index of Object.keys(rows).filter(isInteger)) {
+        tr = rows[index];
+        const $ = cheerio.load(tr);
+
+        // It's the date header
+        if (tr.attribs.class == 'success') {
+          continue;
+          // It's a match
+        } else {
+          response.push(parseHtmlRowToJson($, tr));
+        }
+
+      }
+
+      if (req != undefined && req.body != undefined) {
+        response = response.filter(node => filterResults(node, req.body));
+      }
+
+      res.status(200).send(response);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
+
+};
+
+function filterResults(json, body) {
+  if (body != undefined) {
+
+    if (body.time != undefined && json.time != body.time) {
+      return false;
+    }
+
+    if (body.date != undefined && json.date != body.date) {
+      return false;
+    }
+
+    if (body.sport != undefined && json.sport != undefined && !json.sport.toUpperCase().includes(body.sport.toUpperCase())) {
+      return false;
+    }
+
+    if (body.match != undefined && json.match != undefined && !json.match.toUpperCase().includes(body.match.toUpperCase())) {
+      return false;
+    }
+
+    if (body.league != undefined && json.league != undefined && !json.league.toUpperCase().includes(body.league.toUpperCase())) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isInteger(string) {
+  return string.match(/^-{0,1}\d+$/);
+}
+
+function parseHtmlRowToJson($, tr) {
+  res = {
+    links: [],
+    time: $('td', tr).get(1).children[0].data.trim(),
+    date: $('td', tr).get(2).children[0].data.trim(),
+    sport: $('td', tr).get(3).children[0].data.trim(),
+    match: $('td', tr).get(5).children[0].data.trim(),
+    league: $('td', tr).get(6).children[0].data.trim()
+  };
+
+  linksHtml = $('td > a', tr);
+
+  for (i = 0; i < linksHtml.length; i++) {
+    link = {
+      name: linksHtml.get(i).children[0].data.trim(),
+      url: linksHtml.get(i).attribs.href.replace('acestream://', acestreamHostStreamUrl)
+    };
+    res.links.push(link);
+  }
+
+  return res;
+}
